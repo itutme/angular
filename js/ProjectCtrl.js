3b1946219aab9ce9f50b6d2d1352c390f0708760
Subin <sthasbin@jaljal.com>
2015-09-07 17:06:28 +0545

    watch initial commit

--- js/ProjectCtrl.js
+++ js/ProjectCtrl.js
@@ -1,4 +1,9 @@
 var PmApp = angular.module('ProjectMgmt', []);
 PmApp.controller('ProjectCtrl', ['$scope', function ($scope) {
     $scope.project = {'title':'Mobile App Development','startDate':'08/20/2015','endDate':'12/31/2015'};
+
+    $scope.count = {'title':0, 'startDate':0, 'endDate':0};
+    $scope.$watch("project.title", function(){
+        $scope.count.title++;
+    });
 }]);
