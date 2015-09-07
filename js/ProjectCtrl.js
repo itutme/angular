b6768d430040a37e1c5aa948d544591dcc1a721b
Subin <sthasbin@jaljal.com>
2015-09-07 17:54:39 +0545

    example add new users initial commit

--- js/ProjectCtrl.js
+++ js/ProjectCtrl.js
@@ -1,4 +1,3 @@
-var PmApp = angular.module('ProjectMgmt', []);
 PmApp.controller('ProjectCtrl', ['$scope', function ($scope) {
     $scope.projects = [{'title':'Mobile App Development','startDate':'08/20/2015','endDate':'12/31/2015'},{'title':'Angular web App','startDate':'09/15/2015','endDate':'01/25/2016'},{'title':'Mobile Game','startDate':'08/30/2015','endDate':'12/31/2015'}];
 
---
+++ js/UserCtrl.js
@@ -0,0 +1,8 @@
+PmApp.controller('UserCtrl', ['$scope', function ($scope) {
+$scope.users = [];
+    $scope.addUser = function(user){
+        var newUser = {'username':user.username,'email':user.email,'password':user.password};
+        $scope.users.push(newUser);
+        $scope.user = "";
+    }
+}]);
