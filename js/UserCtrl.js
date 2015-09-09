fa48e35210aaae0a113add205fca31baaa71eaf6
Subin <sthasbin@jaljal.com>
2015-09-09 10:21:19 +0545

    userctrl.js update

--- js/UserCtrl.js
+++ js/UserCtrl.js
@@ -1,6 +1,6 @@
 PmApp.controller('UserCtrl', ['$scope', function ($scope) {
-$scope.users = [];
-    $scope.addUser = function(valid,user){
+    $scope.users = [];
+    $scope.addUser = function(user){
         var newUser = {'username':user.username,'email':user.email,'password':user.password};
         $scope.users.push(newUser);
         $scope.user = "";
