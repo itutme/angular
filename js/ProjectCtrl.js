db0f220dd1c78283c8b923b3fdcc9eda57da3dba
Subin <sthasbin@jaljal.com>
2015-09-17 16:48:04 +0545

    adding project desc

--- js/ProjectCtrl.js
+++ js/ProjectCtrl.js
@@ -1,4 +1,4 @@
 var PmApp = angular.module('ProjectMgmt', []);
 PmApp.controller('ProjectCtrl', ['$scope', function ($scope) {
-    $scope.project = {'title':'Mobile App Development','startDate':'08/20/2015','endDate':'12/31/2015'};
+    $scope.project = {'title':'Angular Tutorial','description':'Create fast-track Angular Tutorial ', 'startDate':'08/20/2015','endDate':'12/31/2015'};
 }]);
