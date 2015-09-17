36a4b56d0dd06bb21f77d5ebd964103876f301b2
Subin <sthasbin@jaljal.com>
2015-09-17 17:00:40 +0545

    adding project description

--- js/ProjectCtrl.js
+++ js/ProjectCtrl.js
@@ -1,7 +1,8 @@
 PmApp.controller('ProjectCtrl', ['$scope', function ($scope) {
-    $scope.project = {'title':'Mobile App Development','startDate':'08/20/2015','endDate':'12/31/2015'};
+    $scope.project = {'title':'Angular Tutorial','description':'Create fast-track Angular Tutorial ', 'startDate':'08/20/2015','endDate':'12/31/2015'};
 
-    $scope.count = {'title':0, 'startDate':0, 'endDate':0};
+    $scope.count = {'title':0, 'description':0, 'startDate':0, 'endDate':0};
+    
     $scope.$watch("project.title", function(){
         $scope.count.title++;
     });
