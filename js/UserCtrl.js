2f58afa33a336ffb7dbdf8e8d8a745e5c6ea8232
Subin <sthasbin@jaljal.com>
2015-09-18 10:57:05 +0545

    update input fields

--- js/UserCtrl.js
+++ js/UserCtrl.js
@@ -1,7 +1,7 @@
 PmApp.controller('UserCtrl', ['$scope', function ($scope) {
 $scope.users = [];
     $scope.addUser = function(user){
-        var newUser = {'username':user.username,'email':user.email,'password':user.password};
+        var newUser = {'fullName':user.firstName + ' ' + user.lastName, 'username':user.username, 'email':user.email, 'password':user.password};
         $scope.users.push(newUser);
         $scope.user = "";
     }
