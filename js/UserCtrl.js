5460a5a0779979add3363cd7b120559ec8292349
Subin <sthasbin@jaljal.com>
2015-09-18 15:37:42 +0545

    add  on variable

--- js/UserCtrl.js
+++ js/UserCtrl.js
@@ -4,6 +4,6 @@ PmApp.controller('UserCtrl', ['$scope', function ($scope) {
         var newUser = {'fullName':user.firstName + ' ' + user.lastName, 'username':user.username, 'email':user.email, 'password':user.password};
         $scope.users.push(newUser);
         $scope.user = "";
-        console.log(users)
+        console.log($scope.users)
     }
 }]);
