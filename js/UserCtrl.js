e8eb6b6a4925c8f0eac62727a0f123f759a245fc
Subin <sthasbin@jaljal.com>
2015-09-18 12:17:10 +0545

    add novalidate in form

--- js/UserCtrl.js
+++ js/UserCtrl.js
@@ -1,8 +1,9 @@
 PmApp.controller('UserCtrl', ['$scope', function ($scope) {
-$scope.users = [];
+    $scope.users = [];
     $scope.addUser = function(user){
         var newUser = {'fullName':user.firstName + ' ' + user.lastName, 'username':user.username, 'email':user.email, 'password':user.password};
         $scope.users.push(newUser);
         $scope.user = "";
+        console.log(users)
     }
 }]);
