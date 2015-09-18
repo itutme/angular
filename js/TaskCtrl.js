b7743cf8cbeb581c0a5c1b8fb1db27b39f47d6b4
Subin <sthasbin@jaljal.com>
2015-09-18 10:01:01 +0545

    add inputbox for all

--- js/TaskCtrl.js
+++ js/TaskCtrl.js
@@ -1,7 +1,16 @@
 PmApp.controller('TaskCtrl', ['$scope', function ($scope) {
 $scope.tasks = [];
     $scope.addTask = function(task){
-        var newTask = {'projectId':task.projectId,'title':task.title,'assignTo':task.assignTo};
+        var newTask = {
+            'projectId':task.projectId,
+            'title':task.title,
+            'description':task.description,
+            'priority':task.priority,
+            'estHour': task.estHour,
+            'startDate':task.startDate,
+            'endDate':task.endDate,
+            'assignTo':task.assignTo
+        };
         $scope.tasks.push(newTask);
         $scope.task = "";
     }
