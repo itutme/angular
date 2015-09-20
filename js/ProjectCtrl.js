f218dee31e33f903ce92083f4f6fcc1378eb01fe
Subin <sthasbin@jaljal.com>
2015-09-20 13:21:20 +0545

    add validation css and check validation

--- js/ProjectCtrl.js
+++ js/ProjectCtrl.js
@@ -1,9 +1,35 @@
-var PmApp = angular.module('ProjectMgmt', []);
-PmApp.controller('ProjectCtrl', ['$scope', function ($scope) {
-     $scope.projects = [{'title':'angular tutorial', 'description':'Create fast-track Angular Tutorial ', 'startDate':'2015-08-20', 'endDate':'2015-12-31'},{'title':'mobile app development', 'description': 'Development of mobile App using Titanium', 'startDate':'2015-08-02', 'endDate':'2016-02-31'},{'title':'angular web app', 'description':'Develop web app using AngularJs', 'startDate':'2015-09-15', 'endDate':'2016-01-25'}];
+PmApp.controller('ProjectCtrl', ['$scope', function($scope) {
+    $scope.projects = [{
+        'title': 'angular tutorial',
+        'description': 'Create fast-track Angular Tutorial ',
+        'startDate': '2015-08-20',
+        'endDate': '2015-12-31'
+    }, {
+        'title': 'mobile app development',
+        'description': 'Development of mobile App using Titanium',
+        'startDate': '2015-08-02',
+        'endDate': '2016-02-31'
+    }, {
+        'title': 'angular web app',
+        'description': 'Develop web app using AngularJs',
+        'startDate': '2015-09-15',
+        'endDate': '2016-01-25'
+    }];
 
-    $scope.count = {'title':0, 'startDate':0, 'endDate':0};
-    $scope.$watch("project.title", function(){
-        $scope.count.title++;
-    });
+    $scope.projects = [];
+    $scope.addProject = function(project) {
+        if ($scope.newProject.$valid === true) {
+            var newproject = {
+                'title': project.title,
+                'description': project.description,
+                'startDate': project.startDate,
+                'endDate': project.endDate
+            };
+            $scope.projects.push(newproject);
+            $scope.project = "";
+            alert('validation success');
+        } else {
+            alert('Invalid form');
+        }
+    }
 }]);
