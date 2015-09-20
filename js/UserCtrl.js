82ffcfc0e27a4e527108f33493709bb252f1c607
Subin <sthasbin@jaljal.com>
2015-09-20 17:04:16 +0545

    first commit

--- js/UserCtrl.js
+++ js/UserCtrl.js
@@ -1,9 +1,22 @@
-PmApp.controller('UserCtrl', ['$scope', function ($scope) {
-    $scope.users = [];
-    $scope.addUser = function(user){
-        var newUser = {'fullName':user.firstName + ' ' + user.lastName, 'username':user.username, 'email':user.email, 'password':user.password};
-        $scope.users.push(newUser);
-        $scope.user = "";
-        console.log($scope.users)
+PmApp.controller('UserCtrl', ['$scope', function($scope) {
+    $scope.users = [{'fullName': 'john doe',
+                'username': 'john.doe',
+                'email': 'john.doe@gmail.com',
+                'password': 'janedoe'}];
+    $scope.addUser = function(user) {
+        if ($scope.newUser.$valid === true) {
+            var newUser = {
+                'fullName': user.firstName + ' ' + user.lastName,
+                'username': user.username,
+                'email': user.email,
+                'password': user.password
+            };
+            $scope.users.push(newUser);
+            $scope.user = "";
+            console.log($scope.users)
+            alert('validation success');
+        } else {
+            alert('Invalid form');
+        }
     }
 }]);
